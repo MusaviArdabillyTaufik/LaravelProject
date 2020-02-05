bfa33443272f7d5b00af1ff8cfa74e130b292a8d
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-02-05 22:47:18 +0700

    fix model

--- app/Http/Controllers/Member.php
+++ app/Http/Controllers/Member.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
+use App\Users;
 
 class Member extends Controller
 {
     public function index()
     {
-        $members = DB::table('users')->get();
-        // dump($members);
-        return view('members', ['users' => $members]);
+    	$table = 'users';
+    	$fillable = Users::all();
+    	return view ('members', compact('table', 'fillable'));
+
+
+        // $members = DB::table('users')->get();
+        // // dump($members);
+        // return view('members', ['users' => $members]);
     }
 }
--- resources/views/mainlayout.blade.php
+++ resources/views/mainlayout.blade.php
@@ -49,40 +49,7 @@
     </div>
   </nav>
 
-  <!-- Header -->
-  <header class="masthead">
-    <div class="container d-flex h-100 align-items-center">
-      <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 ">RedAlert</h1>
-        <h1 class="mx-auto my-0">Members</h2>
-          
-      </div>
-    </div>
-  </header>
-  
-  <div class="mx-auto text-center" style="margin-bottom: 22px; margin-top: 22px;">
-    <table class="table table-dark" align="center" border="1" style="width: 90%; margin-top: 7px;">
-            <thead>
-                <tr style="text-align: center;">
-                    <th width="80px">Id</th>
-                    <th width="150px">Name</th>
-                    <th width="200px">NIM</th>
-                    <th width="200px">Faculty</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($fillable as $table)
-                <tr style="text-align: center;">
-                    <td>{{ $table->id }}</td>
-                    <td>{{ $table->name }}</td>
-                    <td>{{ $table->nim }}</td>
-                    <td>{{ $table->faculty }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <a href="#" class="btn btn-primary js-scroll-trigger" style="margin-top: 14px">Join Our Comunity</a>
-  </div>
+  @yield('content')
 
   <!-- Footer -->
   <footer class="bg-black small text-center text-white-50">

3bc333d6dbb1867194f24f3d4968699727ab1342
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-03-16 16:32:38 +0700

    view csgo

---
+++ resources/views/userview/csgo/csgohomeview.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('content')
+ 
+  <!-- Header -->
+  <div class="bgcsgo head">
+    <div class=" csgot1">
+      <h1 class="mx-auto my-0">FIND TEAMMATES</h1><br>
+    </div>
+    <div class="csgot2">
+      <h1 class="mx-auto my-0">IMPROVE YOUR SKILLS</h1><br>
+    </div>
+    <div class=" d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <a href="/csgo/players" class="btn btn-csgo">FIND PLAYERS</a><br>
+      </div>
+    </div>
+  </div>
+
+@stop
---
+++ resources/views/userview/csgo/csgoplayersview.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="bgprofile-top">
+        
+    </div>
+    <div class="bgprofile-bottom">
+        <div class="csgoplayers-container">
+            <div class="csgoplayers-wrapper">
+                <h2>CS:GO</h2>
+                @foreach($member as $tbl)
+                <div class="gameprofile-container">
+                    <div class="gameprofile-header">
+                        <div class="name-rank">
+                            <h3>{{ $tbl->Csgo->name }}</h3>
+                            <img src="{{url('assets/img/'.$tbl->Csgo->rank)}}" onerror="this.style.display='none'">
+                        </div>
+                        <div class="server">
+                            <p>{{ $tbl->Csgo->server_region }}</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="role-container">
+                        <div class="mainrole-wrapper">
+                            <h4>Main</h4>
+                            <div class="mainrole-wrapper-img">
+                                <img src="{{url('assets/img/'.$tbl->Csgo->main_role1)}}" onerror="this.style.display='none'">
+                                <img src="{{url('assets/img/'.$tbl->Csgo->main_role2)}}" onerror="this.style.display='none'">
+                                <img src="{{url('assets/img/'.$tbl->Csgo->main_role3)}}" onerror="this.style.display='none'">
+                            </div>
+                        </div>
+                        <div class="tacticalrole-wrapper">
+                            <h4>Tactical</h4>
+                            <div class="tacticalrole-wrapper-img">
+                                <img src="{{url('assets/img/'.$tbl->Csgo->tactical_role1)}}" onerror="this.style.display='none'">
+                                <img src="{{url('assets/img/'.$tbl->Csgo->tactical_role2)}}" onerror="this.style.display='none'">
+                                <img src="{{url('assets/img/'.$tbl->Csgo->tactical_role3)}}" onerror="this.style.display='none'">
+                            </div>
+                        </div>
+                    </div><br>
+                    <div class="friend-code">
+                        <h4>Friend Code</h4>
+                        <h5>{{ $tbl->Csgo->friend_code }}</h5>
+                    </div>
+                </div>
+        	@endforeach
+            </div>
+        </div>
+    </div>
+
+@stop

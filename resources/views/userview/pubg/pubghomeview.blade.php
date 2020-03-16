ebfe4dc2c39eb94c936cdc04a63c0f8e6f71540c
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-03-16 16:32:14 +0700

    view pubg

---
+++ resources/views/userview/pubg/pubghomeview.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('content')
+ 
+  <!-- Header -->
+  <div class="bgpubg head">
+    <div class=" pubgt1">
+      <h1 class="mx-auto my-0">FIND TEAMMATES</h1><br>
+    </div>
+    <div class="pubgt2">
+      <h1 class="mx-auto my-0">REKT YOUR ENEMY</h1><br>
+    </div>
+    <div class=" d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <a href="/pubg/players" class="btn btn-pubg">FIND PLAYERS</a><br>
+      </div>
+    </div>
+  </div>
+
+@stop

28cd15d2d56cac0816b927ac47681645fe4350db
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-03-16 16:34:21 +0700

    custom javascript

---
+++ public/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#paket1").select2({
+       placeholder: "Main Role"
+   });
+});
+$(document).ready(function () {
+    $("#paket2").select2({
+       placeholder: "Tactical Role"
+   });
+});

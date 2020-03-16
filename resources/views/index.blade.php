520e86edefbb09928193e0adee72abe66505baaa
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-03-16 16:33:19 +0700

    view landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,23 +2,34 @@
 
 @section('content')
  
-  <!-- Header -->
   <header class="bgmain head">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
         <h1 class="mx-auto my-0 ">GAME FOR FUN</h1><br>
         <h1 class="mx-auto my-0 ">GAME FOR MONEY</h1><br>
         <h1 class="mx-auto my-0 ">GAME FOR FUTURE</h1><br>
-        <a href="/members" class="btn js-scroll-trigger btn-home">JOIN</a><br>
+        @if (Auth::guest()) 
+        <a href="/registerForm" class="btn js-scroll-trigger btn-home">JOIN</a><br>
+        @else
+        <a href="/profile" class="btn js-scroll-trigger btn-home">PROFILE</a><br>
+        @endif
         <h2>Choose Your Game</h2>
         <div>
             <div class="home-button">
                 <img class="btn-game-index" src="/assets/img/csgo.jpg">
+                @if (Auth::guest()) 
                 <a href="/csgo"><img class="btn-game-index home-hover" src="/assets/img/csgohover.png"></a>
+                @else
+                <a href="/csgo/home"><img class="btn-game-index home-hover" src="/assets/img/csgohover.png"></a>
+                @endif
             </div>
             <div class="home-button">
                 <img class="btn-game-index" src="/assets/img/pubg.jpg">
+                @if (Auth::guest())
                 <a href="/pubg"><img class="btn-game-index home-hover" src="/assets/img/pubghover.png"></a>
+                @else
+                <a href="/pubg/home"><img class="btn-game-index home-hover" src="/assets/img/pubghover.png"></a>
+                @endif
             </div>
         </div>
         <div>
@@ -38,36 +49,5 @@
       </div>
     </div>
   </header>
-
-
-
-    <!-- <div class="mx-auto text-center" style="padding-bottom: 22px; padding-top: 42px;">
-        <table class="table table-dark" align="center" style="width: 90%; margin-top: 7px;">
-            
-            <thead>
-                <tr>
-                    <th width="80px">No</th>
-                    <th width="150px">Name</th>
-                    <th width="200px">Code</th>
-                    <th width="200px">Rank</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $no =1;?>
-                @foreach($showmember as $tbl)
-                <tr> -->
-                    <!-- <td>{{ $tbl->id }}</td> -->
-                    <!-- <td>{{ $no }}</td> -->
-                    <!-- <td><?php echo $no++;?></td>
-                    <td>
-                        <img class="img-table" src="{{url('uploads/'.$tbl->avatar)}}" alt="{{$tbl->avatar}}">&nbsp;&nbsp;{{ $tbl->name }}
-                    </td>
-                    <td>{{ $tbl->code }}</td>
-                    <td>{{ $tbl->rank }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div> -->
 
 @stop

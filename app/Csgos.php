00dc14f71b100f5c67278dba5e6a2d6a1449b3fa
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-03-29 20:05:34 +0700

    model

--- app/Csgos.php
+++ app/Csgos.php
@@ -9,7 +9,7 @@ class Csgos extends Model
     protected $table = 'csgos';
     protected $primaryKey = 'id';
 
-    protected $fillable = ['name', 'main_role1', 'main_role2', 'main_role3', 'tactical_role1', 'tactical_role2', 'tactical_role3', 'rank', 'friend_code', 'server_region'];
+    protected $fillable = ['name', 'main_role1', 'main_role2', 'main_role3', 'tactical_role1', 'tactical_role2', 'tactical_role3', 'rank',  'friend_code', 'server_region'];
 
     public function Members(){
         return $this->hasOne('App\Members', 'id');

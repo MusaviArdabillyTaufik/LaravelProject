8266f776a3ad6a1a0b623951364d7681944162e7
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-04-02 17:59:02 +0700

    view

--- resources/views/addMemberForm.blade.php
+++ resources/views/addMemberForm.blade.php
@@ -6,8 +6,6 @@
     <header class="masthead">
         <div class="container d-flex h-100 align-items-center">
             <div class="mx-auto">
-                <h1 class="mx-auto my-0 text-center">Join Us</h1>
-                <h1 class="mx-auto my-0 text-center">RedAlert</h1>
                 <h1 class="mx-auto my-0 text-center">Members</h2>
                 <form method="POST" action="{{ route('add.Member') }}" enctype="multipart/form-data">
                     @csrf
@@ -17,8 +15,16 @@
                         <input type="text" class="form-control" name="name"><br>
                         <label class="text-light">Code</label>
                         <input type="text" class="form-control" name="code"><br>
+                        <label class="text-light">Age</label>
+                        <input type="text" class="form-control" name="age"><br>
+                        <label class="text-light">Gender</label>
+                        <input type="text" class="form-control" name="gender"><br>
                         <label class="text-light">Rank</label>
                         <input type="text" class="form-control" name="rank"><br>
+                        <label class="text-light">Language</label>
+                        <input type="text" class="form-control" name="language"><br>
+                        <label class="text-light">Additional Info</label>
+                        <input type="text" class="form-control" name="additional_info"><br>
                         <label class="text-light">Avatar</label><br>
                         <input type="file" name="avatarinput" accept=".jpg, .png">
                     </div>
